642867792c1804f8d3aafd728831c44f558889bb
FrancescoC-Unity <francescoc@unity3d.com>
2020-03-18 15:10:03 +0100

    Fix the issue preventing thumbnail to be preserved upon reimport on load

--- com.unity.render-pipelines.high-definition/Editor/Lighting/Reflection/HDCubemapInspector.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/Reflection/HDCubemapInspector.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Experimental.Rendering;
 
@@ -35,26 +36,25 @@ namespace UnityEditor.Rendering.HighDefinition
         public float previewExposure = 0f;
         public float mipLevelPreview = 0f;
 
-        void InitMaterial()
+        void InitMaterialIfNeeded()
         {
-            m_ReflectiveMaterial = new Material(Shader.Find("Debug/ReflectionProbePreview"))
+            if(m_ReflectiveMaterial == null)
             {
-                hideFlags = HideFlags.HideAndDontSave
-            };
-        }
-
-        void Awake()
-        {
-            if (m_ReflectiveMaterial == null)
-                InitMaterial();
+                var shader = Shader.Find("Debug/ReflectionProbePreview");
+                if(shader != null)
+                {
+                    m_ReflectiveMaterial = new Material(Shader.Find("Debug/ReflectionProbePreview"))
+                    {
+                        hideFlags = HideFlags.HideAndDontSave
+                    };
+                }
+            }
         }
 
         void OnEnable()
         {
             if (m_PreviewUtility == null)
                 InitPreview();
-
-            m_ReflectiveMaterial.SetTexture("_Cubemap", target as Texture);
         }
 
         void OnDisable()
@@ -79,7 +79,12 @@ namespace UnityEditor.Rendering.HighDefinition
             if (m_PreviewUtility == null)
                 InitPreview();
 
+            // We init material just before using it as the inspector might have been enabled/awaked before during import.
+            InitMaterialIfNeeded();
+
             UpdateCamera();
+
+            m_ReflectiveMaterial.SetTexture("_Cubemap", target as Texture);
 
             m_PreviewUtility.BeginPreview(r, GUIStyle.none);
             m_PreviewUtility.DrawMesh(sphereMesh, Matrix4x4.identity, m_ReflectiveMaterial, 0);
@@ -199,9 +204,6 @@ namespace UnityEditor.Rendering.HighDefinition
         }
         public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
         {
-            if (m_ReflectiveMaterial == null)
-                InitMaterial();
-
             m_CameraDistance = 1.25f;
             m_CameraPhi = Mathf.PI * 0.33f;
             m_CameraTheta = Mathf.PI;
@@ -210,12 +212,33 @@ namespace UnityEditor.Rendering.HighDefinition
 
             UpdateCamera();
 
+            var previewShader = EditorGUIUtility.LoadRequired("Previews/PreviewCubemap.shader") as Shader;
+            var previewMaterial = new Material(previewShader)
+                {
+                    hideFlags = HideFlags.HideAndDontSave
+                };
+
+            // We need to force it to go through legacy
+            var currentPipelineAsset = GraphicsSettings.renderPipelineAsset;
+            GraphicsSettings.renderPipelineAsset = null;
+
+            previewMaterial.SetVector("_CameraWorldPosition", m_PreviewUtility.camera.transform.position);
+            previewMaterial.SetFloat("_Mip", 0.0f);
+            previewMaterial.SetFloat("_Alpha", 0.0f);
+            previewMaterial.SetFloat("_Intensity", 1.0f);
+            previewMaterial.mainTexture = (target as Texture);
+
             m_PreviewUtility.ambientColor = Color.black;
             m_PreviewUtility.BeginStaticPreview(new Rect(0, 0, width, height));
-            m_PreviewUtility.DrawMesh(sphereMesh, Matrix4x4.identity, m_ReflectiveMaterial, 0);
+            m_PreviewUtility.DrawMesh(sphereMesh, Matrix4x4.identity, previewMaterial, 0);
             m_PreviewUtility.camera.Render();
 
-            return m_PreviewUtility.EndStaticPreview();
+            var outTexture = m_PreviewUtility.EndStaticPreview();
+
+            // Reset back to whatever asset was used before the rendering 
+            GraphicsSettings.renderPipelineAsset = currentPipelineAsset;
+            return outTexture;
+
         }
     }
 }
